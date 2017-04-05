9c9fdeb999221c9af4c99568fb1fe44b0a051f28
Thomi Jasir <thomijasir@gmail.com>
2017-04-05 09:30:41 +0700

    Space
    Merubah Sepasi dengan Character

--- BelahKetupat.java
+++ BelahKetupat.java
@@ -26,18 +26,18 @@ public class BelahKetupat {
         int n = 12/2;
         for(int i=1;i<=n;i++){
             for(int k=n;k>i;k--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int j=1;j<=l;j++){
                 System.out.print("*");
             }
             l+=2;
-            System.out.println("");
+            System.out.println("*");
             }
             l -= 4;
             for(int i=(n-1);i>=1;i--){
                 for(int k=i;k<=(n-1);k++){
-                    System.out.print(" ");
+                    System.out.print("0");
             }
             for(int j=l;j>=1;j--){
                 System.out.print("*");
